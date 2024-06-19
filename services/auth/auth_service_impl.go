c7d243aa4d01ae55c94be5459ded52948e7211fe
jeppung <jeppung@gmail.com>
2024-06-20 03:32:11 +0700

    feat: adding password hashing and signup service error log

--- services/auth/auth_service_impl.go
+++ services/auth/auth_service_impl.go
@@ -1,8 +1,11 @@
 package services
 
 import (
+	"log"
+
 	models "github.com/jeppung/go-notes.git/models"
 	repositories "github.com/jeppung/go-notes.git/repositories/auth"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthServiceImpl struct {
@@ -10,9 +13,18 @@ type AuthServiceImpl struct {
 }
 
 func (s *AuthServiceImpl) Signup(data models.Auth) error {
+	if passwordHash, err := bcrypt.GenerateFromPassword([]byte(data.Password), 10); err != nil {
+		log.Println("(auth_service)(Signup):", err.Error())
+		return err
+	}else{
+		data.Password = string(passwordHash)
+	}
+	
 	if err := s.repository.CreateUser(data); err != nil {
+		log.Println("(auth_service)(CreateUser):", err.Error())
 		return err
 	}
+
 	return nil
 }
 
